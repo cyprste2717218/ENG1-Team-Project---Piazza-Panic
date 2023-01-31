6bb923ebef9e7aea67211dcf8360eb5be1e2756a
conncon99 <conncon@yahoo.co.uk>
2023-01-31 17:06:13 +0000

    fixed issues with lag

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -173,7 +173,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        System.out.println("FPS: " + Gdx.graphics.getFramesPerSecond());
+        //System.out.println("FPS: " + Gdx.graphics.getFramesPerSecond());
         mainMenu.camera.update();
         Gdx.gl.glClearColor(0.89f,0.97f,0.99f,1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
